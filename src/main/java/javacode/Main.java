291118546dbc59e565a4e20857f879f2fb4af47b
b3nni <benjamin_nilsson@hotmail.com>
2021-10-06 15:20:00 +0200

    Changed the packet name and added scenes and images packages

--- src/main/java/javacode/Main.java
+++ src/main/java/javacode/Main.java
@@ -1,4 +1,4 @@
-package Javacode;
+package javacode;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/javacode/Player.java
+++ src/main/java/javacode/Player.java
@@ -1,4 +1,4 @@
-package Javacode;
+package javacode;
 
 public class Player {
 }
