b1d77e41681e118162a61e44833d9e5f9ffeacf2
whokegard <rewelox@hotmail.com>
2021-10-07 10:59:57 +0200

    Börjat pilla med Animal classen, ej klar /W

--- src/main/java/javacode/Animal.java
+++ src/main/java/javacode/Animal.java
@@ -1,4 +1,47 @@
 package javacode;
 
-public class Animal {
+
+enum Gender {
+    MALE, FEMALE
+}
+
+public abstract class Animal {
+    private String name;
+    private int age;
+    private int health;
+    private Gender gender;
+    private int hunger; //Health
+
+    public Animal(String name, int age, Gender gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.hunger = hunger;
+        hunger = 0; //Sets the hunger to 0.
+    }
+
+    public void roundPassed(){
+        hunger++; //Adds 1 hunger after every round.
+    }
+
+    private int getHunger(){
+        return hunger;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 }
