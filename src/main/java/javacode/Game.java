8d024379e09fafbaaadd7ae2aae5c506513c7eb2
xLara <xlaraa@hotmail.se>
2021-10-07 12:10:51 +0200

    Added comments in Game // Lara

--- src/main/java/javacode/Game.java
+++ src/main/java/javacode/Game.java
@@ -5,15 +5,15 @@ import javafx.stage.Stage;
 import java.util.ArrayList;
 
 public class Game {
-    private ArrayList<Player> myPlayerList = new ArrayList<Player>();
-    private int turns;
-    private int maxTurns;
+    private ArrayList<Player> myPlayerList = new ArrayList<Player>();  //arraylist for players
+    private int turns; // tracks each turn
+    private int maxTurns; // declare maximum turns here ? 5-30 turns
 
 
-    public Game() {
+    public Game() {  // remove ?
     }
 
-    public void addPlayer(Player player){
+    public void addPlayer(Player player){  // adds each player into the arraylist
         this.myPlayerList.add(player);
     }
 
