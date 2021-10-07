48da2c3c340b77ab2a360b3cb2b409b00b2f0b53
Fredrik-Siljeblad <fredrik.siljeblad@gmail.com>
2021-10-07 13:15:03 +0200

    in Store - buyAnimal added.

--- src/main/java/javacode/Store.java
+++ src/main/java/javacode/Store.java
@@ -1,12 +1,21 @@
 package javacode;
 
 public class Store {
+    /**
+     * Adds the animal and removes the AnimalBucks for it to and from player.
+     * @param player
+     * @param animal
+     */
     public void buyAnimal(Player player, Animal animal){
-
         player.addAnimal(animal);
         player.setMyMoney(player.getMyMoney() - animal.getValue());
     }
 
+    /**
+     * Adds the food to the player and removes the AnimalBucks for it.
+     * @param player
+     * @param food
+     */
     public void buyFood(Player player, Food food){
         player.addFood(food);
         player.setMyMoney(player.getMyMoney() - food.getValue());
