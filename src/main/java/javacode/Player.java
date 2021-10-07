2f857c62bd1668cb45e47927fbac61f198fdeb84
Fredrik-Siljeblad <fredrik.siljeblad@gmail.com>
2021-10-07 10:15:08 +0200

    Player Class filled out with fields, constructor, getters & setters

--- src/main/java/javacode/Player.java
+++ src/main/java/javacode/Player.java
@@ -1,4 +1,47 @@
 package javacode;
 
+import java.util.ArrayList;
+
 public class Player {
+    private ArrayList<Animal> myAnimals = new ArrayList<>();
+    private ArrayList<Food> myFood = new ArrayList<>();
+    private int myMoney;
+    private String myName;
+
+    public Player(String myName) {
+        this.myMoney = 100; //AnimalBucks
+        this.myName = myName;
+    }
+
+    public ArrayList<Animal> getMyAnimals() {
+        return myAnimals;
+    }
+
+    public void setMyAnimals(ArrayList<Animal> myAnimals) {
+        this.myAnimals = myAnimals;
+    }
+
+    public ArrayList<Food> getMyFood() {
+        return myFood;
+    }
+
+    public void setMyFood(ArrayList<Food> myFood) {
+        this.myFood = myFood;
+    }
+
+    public int getMyMoney() {
+        return myMoney;
+    }
+
+    public void setMyMoney(int myMoney) {
+        this.myMoney = myMoney;
+    }
+
+    public String getMyName() {
+        return myName;
+    }
+
+    public void setMyName(String myName) {
+        this.myName = myName;
+    }
 }
