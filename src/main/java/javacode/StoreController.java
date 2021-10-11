7fbbf38bb1febbec47641b9fd048b4c353b8f88d
b3nni <benjamin_nilsson@hotmail.com>
2021-10-11 13:54:37 +0200

    limited the player choise depending on situation, added prices

--- src/main/java/javacode/StoreController.java
+++ src/main/java/javacode/StoreController.java
@@ -41,12 +41,25 @@ public class StoreController implements Initializable {
     @FXML
     private Tab buyAnimalsTab, buyFoodTab, sellAnimalsTab;
 
-    private Animal animal;
+    //private specis animal;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         moneyText.setText(String.valueOf(Game.getCurrentPlayer().getMyMoney()) + "AB");
 
+        selectTab();
+
+        String species;
+        buyCowButton.setOnMouseClicked(event -> {
+       //     species = "cow";
+            specificationsWindow.setVisible(true);
+        });
+
+       // var myAnimals = Game.getCurrentPlayer().getMyAnimals();
+
+    }
+
+    private void selectTab() {
         var currentTab = Game.getCurrentTab();
         var selectionModel = store.getSelectionModel();
         if (currentTab.equals("buyAnimals")) {
@@ -62,26 +75,17 @@ public class StoreController implements Initializable {
             buyAnimalsTab.setDisable(true);
             buyFoodTab.setDisable(true);
         }
-
-        buyCowButton.setOnMouseClicked(event -> {
-            animal = new Cow("", Gender.FEMALE);
-            specificationsWindow.setVisible(true);
-        });
-
-       // var myAnimals = Game.getCurrentPlayer().getMyAnimals();
-
     }
 
 
     public void openStoreScene(ActionEvent actionEvent) throws Exception {
         //: todo: put store items in gridpanes that we then can make invisible.
-        // todo: maybe a tracker of which move was chosen making the other moves unavilavble
+        // todo: maybe a tracker of which move was chosen making the other moves unavilable
 
 
     }
 
     public void openStoreAfterMoveScene(ActionEvent actionEvent) throws Exception {
-
         if (maleBox.isSelected() && femaleBox.isSelected()) {
             errorEmptyFieldText.setVisible(false);
             errorCheckedText.setVisible(true);
@@ -102,13 +106,21 @@ public class StoreController implements Initializable {
 
         //todo: maybe setters and getters for gender and name, to avoid having to create 10 of these methods;
 
+        //todo: alternatively add two more fields to the grid with one being a textfield and one genderField;
+
         //animal.setName(name)
         //animal.setGender()
         //addAnimal(this animal field)
 
-        animal = new Cow(nameOfAnimalField.getText(), gender);
-        Store.buyAnimal(Game.getCurrentPlayer(), animal);
+
+
+      //  animal = new Cow(nameOfAnimalField.getText(), gender);
+       // Store.buyAnimal(Game.getCurrentPlayer(), animal);
         SceneCreator.launchScene("/scenes/StoreMenuScene.fxml");
+    }
+
+    private Cow typeOfAnimal(String name, Gender gender) {
+        return new Cow(name, gender);
     }
 
     public void openTurnScene(ActionEvent actionEvent) {
